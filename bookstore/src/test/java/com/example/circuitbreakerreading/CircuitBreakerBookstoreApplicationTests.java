bf1297431ff3b2bfaf3d67741735f92c3872cd69
Spyro1001 <raymondhedgecock@gmail.com>
2019-12-04 16:27:35 -0600

    Initial commit

---
+++ bookstore/src/test/java/com/example/circuitbreakerreading/CircuitBreakerBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circuitbreakerreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
